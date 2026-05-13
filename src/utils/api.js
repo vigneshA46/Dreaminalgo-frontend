4c997121b84914a72845cfeafc3c534ad4e81036
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-05-13 09:35:58 +0530

    VDdvd

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const cloudurl = `https://dreaminalgo-backend-production.up.railway.app`
 const domainurl = `https://algoapi.dreamintraders.in`
 
 const api = axios.create({
-   baseURL: `${localurl}`,
+   baseURL: `${domainurl}`,
     withCredentials: true, // 🍪 cookies auto sent 
     headers: { 
       'Content-Type': 'application/json',
